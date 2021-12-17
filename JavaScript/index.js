baeb6a05da776ca7ff3f4e08fa2ecdb0ab89139a
OdySoft <outpost095@gmail.com>
2021-12-17 16:38:02 +0400

    added: max height

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -7,23 +7,32 @@ game.addEventListener('click' , ()=>{
     game_menu.classList.toggle('d-blk');
     setTimeout(()=>{
         game_menu.classList.toggle("transform-85")
-        document.querySelector(".pd").classList.toggle("padding-120")
+        const pd = document.querySelector('.pd')
+        if(pd){
+            pd.classList.toggle("padding-120")
+        }
     }, 5)
 })
 outlast.addEventListener('click' , ()=>{
     const resp_cont = document.querySelector(".responsive-cont")
     resp_cont.classList.toggle("game-padding")
+
     const delta = document.querySelector(".delta")
     delta.classList.toggle('none')
+
     const outlastMenu = document.querySelector('.game-outlast');
     outlastMenu.classList.toggle("d-blk")
+
     document.querySelector('.back').addEventListener('click' , () =>{
         outlastMenu.classList.remove('d-blk')
         delta.classList.remove('none')
+
         resp_cont.classList.remove("game-padding")
     })
 })
 menu.addEventListener('click' , ()=>{
     menu.classList.toggle('active-menu')
-    document.querySelector(".nav-menu").classList.toggle('none')
+    const gm = document.querySelector(".nav-menu")
+    gm.classList.toggle('none')
+    gm.classList.toggle("bg-clr")
 })
